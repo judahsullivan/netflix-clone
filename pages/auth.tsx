caaa8dc381c6b3b36989aa9d7cc2823b14d103eb
judahsullivan <sullivan.judah95@gmail.com>
2023-03-31 17:23:09 -0500

    fixed auth sign in and registration

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,12 +1,33 @@
 import axios from 'axios';
 import { useCallback, useState } from 'react';
+import { NextPageContext } from 'next';
+import { getSession, signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 import Input from '@/components/input';
-import {signIn } from 'next-auth/react'
-import { FaGithub} from 'react-icons/fa'
-import {FcGoogle} from 'react-icons/fc'
+
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 const Auth = () => {
-  
+  const router = useRouter();
+
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -17,17 +38,20 @@ const Auth = () => {
     setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
   }, []);
 
-const login = useCallback(async () => {
-try{
-await signIn("credentials",{
-    email, 
-    password, 
-    callbackUrl: '/profiles'
-});
-}catch(error){
-    console.log(error);
-}
-  },[email,password])
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      });
+
+      router.push('/profiles');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
 
   const register = useCallback(async () => {
     try {
@@ -36,14 +60,12 @@ await signIn("credentials",{
         name,
         password
       });
-      login();
 
+      login();
     } catch (error) {
         console.log(error);
     }
-  }, [email, name, password,login ]);
-  
-;
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -81,19 +103,18 @@ await signIn("credentials",{
                 onChange={(e: any) => setPassword(e.target.value)} 
               />
             </div>
-            <button onClick={ variant === 'login' ? login: register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
-            <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
-            <div onClick={() => signIn('google', {callbackUrl: '/profiles'})} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition' >
-              <FcGoogle size={20}/>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={32} />
+              </div>
+              <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FaGithub size={32} />
+              </div>
             </div>
-             <div onClick={() => signIn('github', {callbackUrl: '/profiles'})} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition' >
-              <FaGithub size={20}/>
-            </div>
-
-            </div>
-           <p className="text-neutral-500 mt-12">
+            <p className="text-neutral-500 mt-12">
               {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
               <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
                 {variant === 'login' ? 'Create an account' : 'Login'}
